8679b173e39d11583e197f4e6ae0b565d60b2469
domkatbess <domkatbess@gmail.com>
2020-09-06 23:57:50 +0100

    first commit

---
+++ src/test/java/com/java/devops/javadevops/JavaDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.devops.javadevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
